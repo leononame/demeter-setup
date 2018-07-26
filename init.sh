572aef089e6833a3672a263d330f6dbf30537a06
Leo <leononame@lho.io>
2018-07-26 21:06:41 +0200

    Update ssh config

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-useradd leo
+adduser leo
+adduser leo sudo
 mkdir /home/leo/.ssh
 cp /root/.ssh/authorized_keys /home/leo/.ssh/authorized_keys
 chown leo:leo /home/leo/.ssh/authorized_keys
 
+echo "Replace the following in /etc/sudoers\n%sudo   ALL=(ALL:ALL) ALL\nwith\n%sudo  ALL=(ALL) NOPASSWD:ALL"
