47296be0b998132ec8ee40e2205439b19fa5e8d7
Leo <leononame@lho.io>
2018-07-26 21:30:37 +0200

    remember me

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ cp /root/.ssh/authorized_keys /home/leo/.ssh/authorized_keys
 chown -R leo:leo /home/leo/.ssh
 
 echo "Replace the following in /etc/sudoers\n%sudo   ALL=(ALL:ALL) ALL\nwith\n%sudo  ALL=(ALL) NOPASSWD:ALL"
+
+apt install docker.io
+docker pull rocket.chat
