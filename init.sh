55b9663b569e5b8259373f8e048fffe9acbdd948
Leo <leononame@lho.io>
2018-07-26 21:11:09 +0200

    recursively own .ssh to leo

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ adduser leo
 adduser leo sudo
 mkdir /home/leo/.ssh
 cp /root/.ssh/authorized_keys /home/leo/.ssh/authorized_keys
-chown leo:leo /home/leo/.ssh/authorized_keys
+chown -R leo:leo /home/leo/.ssh
 
 echo "Replace the following in /etc/sudoers\n%sudo   ALL=(ALL:ALL) ALL\nwith\n%sudo  ALL=(ALL) NOPASSWD:ALL"
