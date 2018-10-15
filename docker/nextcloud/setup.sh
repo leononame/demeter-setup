265b18f5fc054566392a1531da0f2ac4bf1e088e
Leo <leononame@lho.io>
2018-10-15 18:49:27 +0200

    Add nextcloud setup

---
+++ docker/nextcloud/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /var/www/nextcloud
+cp docker-compose.yml /var/www/nextcloud
+
+# setup nginx
+ln -s -T /etc/nginx/sites-available/nextcloud /etc/nginx/sites-enabled/nextcloud
+rm /etc/nginx/sites-enabled/default
+
+echo "Launching docker containers for Nextcloud. This operation may take a while."
+
+sudo service nginx restart
+cd /var/www/nextcloud
+docker-compose up -d
